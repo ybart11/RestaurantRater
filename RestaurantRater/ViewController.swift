3bf155b5e28239b206c08db897eed031fe4c0c23
yovany_23 <yovany1061@gmail.com>
2023-04-04 11:44:48 -0400

    Initial Commit

---
+++ RestaurantRater/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantRater
+//
+//  Created by Yovany Bartolome on 4/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
