982a09c5b6347cddf9ed0c34ed92d731c77f8e5d
yovany_23 <yovany1061@gmail.com>
2023-04-05 01:10:18 -0400

    re-dragged restaurant name label from RateDish Scene to change the name

--- RestaurantRater/RateDishViewController.swift
+++ RestaurantRater/RateDishViewController.swift
@@ -6,26 +6,29 @@
 //
 
 import UIKit
+import CoreData
 
 
 protocol RateDishViewControllerDelegate: AnyObject {
-    func didSaveEntree()
+    
 }
 
 class RateDishViewController: UIViewController {
     
-    
-    
-    @IBOutlet weak var restaurantName: UILabel!
+   
+    @IBOutlet weak var restaurantNameLabel: UILabel!
     @IBOutlet weak var txtDishName: UITextField!
     @IBOutlet weak var txtDishType: UITextField!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    var currentEntree: Entree?
     
     // Add a property to hold the restaurant name
     var restaurantNameHolder: String?
         
     weak var delegate: RateDishViewControllerDelegate?
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,9 +36,29 @@ class RateDishViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         // Set the restaurant name label text to the restaurant name passed from MainViewController
-        restaurantName.text = restaurantNameHolder
+        restaurantNameLabel.text = restaurantNameHolder
         
+        // Add a save button to the navigation bar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveEntree))
+
         
     }
+    
+    @objc func saveEntree() {
+        guard let restaurantName = restaurantNameHolder, !restaurantName.isEmpty else {
+                return
+            }
+            
+            let context = appDelegate.persistentContainer.viewContext
+            
+            if currentEntree == nil {
+                currentEntree = Entree(context: context)
+            }
+        
+        currentEntree?.ename = txtDishName.text
+        currentEntree?.etype = txtDishType.text
+        currentEntree?.erating = Int16(segmentedControl.selectedSegmentIndex)
 
+        appDelegate.saveContext()
+    }
 }
